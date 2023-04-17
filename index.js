db7e0151c28b8ca4055e954de77412bdee190aab
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-04-18 09:30:54 +1000

    Coded questions for prompt at index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("test")
+const questions = [{
+    type: 'input',
+    name: 'text',
+    message: 'What is text for the logo? (Must not be more than 3 characters.)',
+},
+{
+    type: 'input',
+    name: 'text-color',
+    message: 'Which color do you choose for the text?',
+},
+{
+    type: 'list',
+    name: 'shape',
+    message: 'Choose which shape for the logo.',
+    choices: ['Triangle', 'Circle', 'Square'],
+
+},
+{
+    type: 'input',
+    name: 'shape-color',
+    message: 'Which color do you choose for the shape?',
+
+},
+]
