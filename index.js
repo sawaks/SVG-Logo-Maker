1cedeed843a3155947524041813b8d2aaee5ee06
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-04-18 09:55:03 +1000

    Coded packages needed for this application and some functions for rendering the file for result

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const { readFile, writeFile } = require('fs/promises');
+const inquirer = require('inquirer');
+const renderSVG = require('./liv/shapes.js');
+
+
 const questions = [{
     type: 'input',
     name: 'text',
@@ -22,3 +27,24 @@ const questions = [{
 
 },
 ]
+
+function writeToFile(fileName, data) {
+    return inquirer.prompt(data).then((data) => {
+        const svgOutput = renderSVG.render(data)
+        console.log(svgOutput);
+
+        writeFile(fileName, svgOutput, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('file writing successful');
+            }
+        });
+    })
+}
+
+function init() {
+    writeToFile('./examples/result.svg', questions);
+}
+
+init();
